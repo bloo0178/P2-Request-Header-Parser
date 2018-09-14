f5c3911834bc415d2e53d8fc68c92e556880cf02
Glitch (p2-request-header-parser) <none>
2018-09-14 12:18:37 +0000

    🐿🎃 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.enable('trust proxy');
+
+// Mount the body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,12 +23,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function(req, res) {
+  res.json({ipaddress: req.ip, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+});
 
 
 // listen for requests :)
